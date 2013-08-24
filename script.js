fc06ddf65ee1b2f6334920c7dc02b2fef27a929b
biaobiaoqi <shenyapeng@gmail.com>
2013-08-24 16:03:40 +0800

    Make section click event support ro scrolling movement

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var Resume = function(window, document) {
     var step;
 
     var scroll = function() {
+
         var temp;
         if(window.innerWidth < 650 || start == document.documentElement.scrollTop + document.body.scrollTop) {
             window.location.hash = hash;
@@ -31,9 +32,10 @@ var Resume = function(window, document) {
         Array.prototype.forEach.call(sect, function(s, i) { //add section nav to each title of section
             ul.innerHTML += '<li><a href="#' + s.id + '">' + s.querySelector('h2').innerHTML + '</a></li>';
             s.addEventListener('click', function(e) { //click for section in navbar
-                setTimeout(function() {
-                    window.location.hash = '#' + s.id;
-                }, 10);
+                dest = document.getElementById(s.id).offsetTop; //get offset of element
+                hash = '#' + s.id;
+                clearTimeout(step);
+                scroll();
             }, false);
         });
         nav.appendChild(ul);
